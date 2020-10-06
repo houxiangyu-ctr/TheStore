ac4746c6032d21ca093550fd537e0e3dad300b39
lvbu123 <246977330@qq.com>
2020-10-06 17:51:58 +0800

    fd

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hello msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Hello from './components/Hello'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hello
   }
 }
 </script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+<div>111</div>
+</template>
+
+<script>
+// const Foo = { template: `<div>foo</div>` };
+// const Too = { template: `<div>too</div>` };
+// const routers = [
+//   { path: "./foo", component: "Foo" },
+//   { path: "./too", component: "Too" },
+// ];
+// const router = VueRouter({
+//   routers,
+// });
+// const app = new Vue({ router }).$mount("#app");
+// export default {
+// };
+</script>
+
+<style>
+</style>
